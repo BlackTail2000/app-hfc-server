79173e3b077b25351e79bfc0dd3222fd3d811c62
BlackTail2000 <carlosgabrielbacamanrique@hotmail.com>
2024-10-12 01:30:42 -0500

    Servidor Habilitado

--- src/main/java/pe/edu/cibertec/app_hfc_server/AppHfcServerApplication.java
+++ src/main/java/pe/edu/cibertec/app_hfc_server/AppHfcServerApplication.java
@@ -2,7 +2,9 @@ package pe.edu.cibertec.app_hfc_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class AppHfcServerApplication {
 
